47a6340dcf0cdd99c8c6a0d794793f2709e7c237
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-11-05 21:04:08 +0100

    add class-validator

--- src/resolvers/trip.resolver.ts
+++ src/resolvers/trip.resolver.ts
@@ -23,39 +23,30 @@ import { sendRefreshToken } from "./sendRefreshToken";
 
 @Resolver((_of) => Trip)
 export class TripResolver {
-  // @Mutation(() => Trip, { name: "updateTrip" })
-  // async updateTrips(): Promise<Trip> {
-  //   console.log("resolver atteint");
-  //   const coords = await getCoordinate();
-  //   console.log("🤩coords", coords);
-  //   const updatedTrip = await TripModel.updateOne(
-  //     { _id: "63627a16ad3d7a6d9999e8e9" },
-  //     {
-  //       $push: {
-  //         locations: {
-  //           latitude: Number(coords.coords[0]),
-  //           longitude: Number(coords.coords[1]),
-  //           date: coords.date,
-  //           name: coords.currentPort,
-  //           description: "description",
-  //         },
-  //       },
-  //     },
-  //     { new: true }
-  //   );
-  //   const refreshedTrip = await TripModel.findOne({
-  //     _id: "63627a16ad3d7a6d9999e8e9",
-  //   });
-  //   console.log("updatedTrip", refreshedTrip);
-  //   return refreshedTrip;
-  // }
-  //   @Mutation(() => Boolean)
-  //   async revokeRefreshTokensForUser(@Arg('userId', () => String) userId: string) {
-  //     const users = await UsersModel.findByIdAndUpdate(
-  //       { _id: userId },
-  //       { $inc: { tokenVersion: 1 } },
-  //       { new: true },
-  //     )
-  //     return true
-  //   }
+  @Mutation(() => Trip, { name: "updateTrip" })
+  async updateTrips(): Promise<Trip> {
+    console.log("resolver atteint");
+    const coords = await getCoordinate();
+    console.log("🤩coords", coords);
+    const updatedTrip = await TripModel.updateOne(
+      { _id: "63627a16ad3d7a6d9999e8e9" },
+      {
+        $push: {
+          locations: {
+            latitude: Number(coords.coords[0]),
+            longitude: Number(coords.coords[1]),
+            date: coords.date,
+            name: coords.currentPort,
+            description: "description",
+          },
+        },
+      },
+      { new: true }
+    );
+    const refreshedTrip = await TripModel.findOne({
+      _id: "63627a16ad3d7a6d9999e8e9",
+    });
+    console.log("updatedTrip", refreshedTrip);
+    return refreshedTrip;
+  }
 }
